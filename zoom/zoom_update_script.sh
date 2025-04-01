448277884ea3dd2e44e7c392a8eb7b484a099e5a
muncjack <muncjack@github.com>
2025-04-01 18:15:52 +0100

    added a release file to the repo and did some clean-up

--- zoom/zoom_update_script.sh
+++ zoom/zoom_update_script.sh
@@ -7,8 +7,7 @@ SCRIPT_DIR="${BASE_DIR}/scripts"
 SCRIPT_MIRROR_DIR="${BASE_DIR}/mirror/zoom"
 SCRIPT_URL="https://raw.githubusercontent.com/muncjack/tools/refs/heads/main/zoom/${MY_NAME}"
 LOCAL_REPO_DIR="${BASE_DIR}/repo/zoom_repo"
-REPO_LIST_FILE="/etc/apt/sources.list.d/zoom.list"
-GPG_KEY_URL="https://zoom.us/linux/gpg-release-key.asc"
+REPO_LIST_FILE="/etc/apt/sources.list.d/zoom.sources"
 GPG_KEY_URL="https://zoom.us/linux/download/pubkey"
 ZOOM_URL="https://zoom.us/client/latest/zoom_amd64.deb"
 GPG_KEY_FILE="/usr/share/keyrings/zoom-archive-keyring.gpg"
@@ -30,17 +29,19 @@ setup_config() {
   
   if [ ! -f "${GPG_KEY_FILE}" ]; then
       echo -e "adding Zoom GPG key\t" 
-      #${BASE_CMD} gpg --dearmor -o /usr/share/keyrings/zoom-archive-keyring.gpg <(curl -fsSL "$GPG_KEY_URL") &&
-      #${BASE_CMD} gpg --dearmor -o /usr/share/keyrings/zoom-archive-keyring.gpg <(wget -O - "$GPG_KEY_URL") &&
       wget -q -O - "$GPG_KEY_URL" |${BASE_CMD} gpg --dearmor -o ${GPG_KEY_FILE} &&
       echo -e "done" || fail_exit 2
   fi
   if [ ! -f "$REPO_LIST_FILE" ]; then
-      DEB_SOURCE_STR="deb [signed-by=${GPG_KEY_FILE}] file://$LOCAL_REPO_DIR ./"
-      echo -e "adding repo config to apt\t" 
-      #${BASE_CMD} tee "$REPO_LIST_FILE" > /dev/null <(echo "deb file://$LOCAL_REPO_DIR ./") &&
-      echo "${DEB_SOURCE_STR}"| ${BASE_CMD} tee "$REPO_LIST_FILE" > /dev/null &&
-      echo -e "done" || fail_exit 3
+      echo -e "adding repo config to apt sources\t" 
+      (
+        echo "Types: deb"
+        echo "URIs: file:$LOCAL_REPO_DIR"
+        echo "Suites: ./"
+        echo "#Components: ./"
+        echo "Trusted: yes"
+        echo "Signed-By: ${GPG_KEY_FILE}"
+      )| ${BASE_CMD} tee "$REPO_LIST_FILE" > /dev/null && echo -e "done" || fail_exit 3
   fi
   if [ ! -f "${SCRIPT_DIR}/${MY_NAME}" ]; then
       echo -e "adding script dir\t" 
@@ -57,7 +58,7 @@ setup_config() {
 script_download() {
   echo -e "download script\t\t"
   ${BASE_CMD} wget -q -N -P "${SCRIPT_MIRROR_DIR}" "${SCRIPT_URL}" && echo -e "done" || fail_exit 1
-  if [ "`sum -r ${SCRIPT_DIR}/.${MY_NAME}.new 2>/dev/null`" != "sum -r ${SCRIPT_DIR}/.${MY_NAME}.new" ]; then
+  if [ "`sum -r ${SCRIPT_DIR}/.${MY_NAME}.new 2>/dev/null`" != "`sum -r ${SCRIPT_MIRROR_DIR}/.${MY_NAME}.new/${MY_NAME}`" ]; then
       # this is for currently runing process to not fail
       ${BASE_CMD} mv -v "${SCRIPT_DIR}/${MY_NAME}" "${SCRIPT_DIR}/.${MY_NAME}.old"
       ${BASE_CMD} cp "${SCRIPT_MIRROR_DIR}/${MY_NAME}" "${SCRIPT_DIR}/${MY_NAME}"
@@ -69,14 +70,19 @@ script_download() {
 }
 
 package_download() {
+  BEFORE_SUM="`sum -r ${LOCAL_REPO_DIR}/zoom_amd64.deb`"
   echo -e "download/check package download\t"
   ${BASE_CMD} wget -q -N -P "$LOCAL_REPO_DIR" "$ZOOM_URL" && echo -e "done" || fail_exit 4
-  cd ${LOCAL_REPO_DIR}
-  #exit 0
-  echo -e "create package repolist\t"
-  #${BASE_CMD} tee Packages <(${BASE_CMD} apt-ftparchive packages zoom_amd64.deb) && echo -e "done" || fail_exit 5
-  ${BASE_CMD} apt-ftparchive packages zoom_amd64.deb |${BASE_CMD} tee  Packages && echo -e "done" || fail_exit 5
-  #${BASE_CMD} apt update
+  if [ "${BEFORE_SUM}" != "`sum -r ${LOCAL_REPO_DIR}/zoom_amd64.deb`" ]; then
+      cd ${LOCAL_REPO_DIR}
+      #exit 0
+      echo -e "(re)create Package file\t"
+      #${BASE_CMD} apt-ftparchive packages zoom_amd64.deb |${BASE_CMD} tee Packages >/dev/null && echo -e "done" || fail_exit 5
+      ${BASE_CMD} apt-ftparchive packages . |${BASE_CMD} tee Packages >/dev/null && echo -e "done" || fail_exit 5
+      echo -e "(re)create Release file \t"
+      ${BASE_CMD} apt-ftparchive release . | ${BASE_CMD} tee Release >/dev/null && echo -e "done" || fail_exit 6
+      #${BASE_CMD} apt update
+  fi 
 }
 
 # Main function
