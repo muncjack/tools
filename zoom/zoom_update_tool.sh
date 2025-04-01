736c540fe3e5983170d7b7569acdc80090ad1f01
muncjack <pete@bilbo.local>
2025-04-01 09:04:17 +0100

    First full version with self update

--- zoom/zoom_update_tool.sh
+++ zoom/zoom_update_tool.sh
@@ -4,6 +4,7 @@
 MY_NAME="c"
 BASE_DIR="/opt/tools"
 SCRIPT_DIR="${BASE_DIR}/scripts"
+SCRIPT_URL="https://github.com/muncjack/tools/blob/main/zoom/zoom_update_tool.sh"
 LOCAL_REPO_DIR="${BASE_DIR}/repo/zoom_repo"
 SCRIPT_DIR="`dirname ${LOCAL_REPO_DIR}`/scripts"
 REPO_LIST_FILE="/etc/apt/sources.list.d/zoom.list"
@@ -12,8 +13,7 @@ GPG_KEY_URL="https://zoom.us/linux/download/pubkey"
 ZOOM_URL="https://zoom.us/client/latest/zoom_amd64.deb"
 GPG_KEY_FILE="/usr/share/keyrings/zoom-archive-keyring.gpg"
 
-if [ "${USER}" == "root" ]
-  then
+if [ "${USER}" == "root" ]; then
     BASE_CMD=""
 else
     BASE_CMD="sudo"
@@ -28,57 +28,67 @@ setup_config() {
   echo -e "Repository directory created \t"
   ${BASE_CMD} mkdir -p "$LOCAL_REPO_DIR" && echo -e "done" || fail_exit 1
   
-  if [ ! -f "/usr/share/keyrings/zoom-archive-keyring.gpg" ]
-    then
-       echo -e "adding Zoom GPG key\t" 
-       #${BASE_CMD} gpg --dearmor -o /usr/share/keyrings/zoom-archive-keyring.gpg <(curl -fsSL "$GPG_KEY_URL") &&
-       #${BASE_CMD} gpg --dearmor -o /usr/share/keyrings/zoom-archive-keyring.gpg <(wget -O - "$GPG_KEY_URL") &&
-       wget -q -O - "$GPG_KEY_URLfsdfdfdsf" |${BASE_CMD} gpg --dearmor -o ${GPG_KEY_FILE} &&
-       echo -e "done" || fail_exit 2
+  if [ ! -f "/usr/share/keyrings/zoom-archive-keyring.gpg" ]; then
+      echo -e "adding Zoom GPG key\t" 
+      #${BASE_CMD} gpg --dearmor -o /usr/share/keyrings/zoom-archive-keyring.gpg <(curl -fsSL "$GPG_KEY_URL") &&
+      #${BASE_CMD} gpg --dearmor -o /usr/share/keyrings/zoom-archive-keyring.gpg <(wget -O - "$GPG_KEY_URL") &&
+      wget -q -O - "$GPG_KEY_URLfsdfdfdsf" |${BASE_CMD} gpg --dearmor -o ${GPG_KEY_FILE} &&
+      echo -e "done" || fail_exit 2
   fi
-  if [ ! -f "$REPO_LIST_FILE" ]
-    then
-       DEB_SOURCE_STR="deb [signed-by=${GPG_KEY_FILE}] file://$LOCAL_REPO_DIR ./"
-       echo -e "adding repo config to apt\t" 
-       #${BASE_CMD} tee "$REPO_LIST_FILE" > /dev/null <(echo "deb file://$LOCAL_REPO_DIR ./") &&
-       echo "${DEB_SOURCE_STR}"| {BASE_CMD} tee "$REPO_LIST_FILE" > /dev/null &&
-       echo -e "done" || fail_exit 3
+  if [ ! -f "$REPO_LIST_FILE" ]; then
+      DEB_SOURCE_STR="deb [signed-by=${GPG_KEY_FILE}] file://$LOCAL_REPO_DIR ./"
+      echo -e "adding repo config to apt\t" 
+      #${BASE_CMD} tee "$REPO_LIST_FILE" > /dev/null <(echo "deb file://$LOCAL_REPO_DIR ./") &&
+      echo "${DEB_SOURCE_STR}"| {BASE_CMD} tee "$REPO_LIST_FILE" > /dev/null &&
+      echo -e "done" || fail_exit 3
   fi
-  if [ ! -f "${SCRIPT_DIR}/${MY_NAME}" ]
-    then
-        echo -e "adding script dir\t" 
-        ${BASE_CMD} mkdir -p "${SCRIPT_DIR}"  && echo -e "done" || fail_exit 1
-        echo -e "download script\t\t"
-        ${BASE_CMD} wget -q https://github.com/muncjack/tools/blob/main/zoom/zoom_update_tool.sh -O "${SCRIPT_DIR}/${MY_NAME}.new" && echo -e "done" || fail_exit 1
-        mv -v "${SCRIPT_DIR}/${MY_NAME}.new" "${SCRIPT_DIR}/${MY_NAME}"
-        chown -R root:root ${BASE_DIR}
-        chmod 555 ${SCRIPT_DIR}/${MY_NAME} 
-   fi
+  if [ ! -f "${SCRIPT_DIR}/${MY_NAME}" ]; then
+      echo -e "adding script dir\t" 
+      ${BASE_CMD} mkdir -p "${SCRIPT_DIR}"  && echo -e "done" || fail_exit 1
+      ${BASE_CMD} chown -R root:root ${BASE_DIR}
+  fi
+  script_download
   package_download
 }
 
+script_download() {
+  echo -e "download script\t\t"
+  ${BASE_CMD} wget -q "${SCRIPT_URL}" -O "${SCRIPT_DIR}/.${MY_NAME}.new" && echo -e "done" || fail_exit 1
+  if [ "`sum -r ${SCRIPT_DIR}/.${MY_NAME}.new`" != "sum -r ${SCRIPT_DIR}/.${MY_NAME}.new" ]; then
+      # this is for currently runing process to not fail
+      ${BASE_CMD} mv -v "${SCRIPT_DIR}/${MY_NAME}.new" "${SCRIPT_DIR}/.${MY_NAME}.old"
+      ${BASE_CMD} cp "${SCRIPT_DIR}/${MY_NAME}.new" "${SCRIPT_DIR}/${MY_NAME}"
+      # set flag to void loop on exec reload of new self
+      export SCRIPT_RESTART="1"
+  fi
+  ${BASE_CMD} chmod 555 "${SCRIPT_DIR}/${MY_NAME} "
+  exec "${SCRIPT_DIR}/${MY_NAME}"
+}
 
 package_download() {
   echo -e "doing itial package download\t"
   ${BASE_CMD} wget -N -P "$LOCAL_REPO_DIR" "$ZOOM_URL" && echo -e "done" || fail_exit 4
-
   cd ${LOCAL_REPO_DIR}
   #exit 0
   echo -e "create package repolist\t"
   #${BASE_CMD} tee Packages <(${BASE_CMD} apt-ftparchive packages zoom_amd64.deb) && echo -e "done" || fail_exit 5
   ${BASE_CMD} apt-ftparchive packages zoom_amd64.deb |${BASE_CMD} tee  Packages && echo -e "done" || fail_exit 5
-  ${BASE_CMD} apt update
+  #${BASE_CMD} apt update
 }
 
 # Main function
 main() {
-    if [ ! -f  "${LOCAL_REPO_DIR}" ]
-      then
-        setup_config
-        ${BASE_CMD} apt install zoom
-    else
-      package_download
-    fi
+  if [ -n "${SCRIPT_RESTART}" ]; then
+    # we have reloaded after update, so just do the package download 
+    package_download
+  elif [ ! -f  "${LOCAL_REPO_DIR}" ]; then
+    setup_config
+    ${BASE_CMD} apt install zoom
+  else
+    # self update if needed
+    script_download 
+    package_download
+  fi
 }
 
 # Run the main function
